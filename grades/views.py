5edab47f9f4b5c7229247481676d861e363bb285
Slade Lim <sladelim@gmail.com>
2023-10-26 17:01:56 -0600

    Finished phase 4

--- grades/views.py
+++ grades/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.shortcuts import render
 from . import models
 
@@ -6,10 +7,26 @@ def assignments(request):
     all_assignments = models.Assignment.objects.order_by("deadline")
     return render(request, "assignments.html", {'assignments': all_assignments})
 def index(request, assignment_id):
-    return render(request, "index.html")
+    try:
+        asgn = models.Assignment.objects.get(pk=assignment_id)
+        total_submissions = asgn.submission_set.count()
+        submissions_assigned_to_you = asgn.submission_set.filter(grader__username="ta1").count()
+        total_students = models.Group.objects.get(name="Students").user_set.count()
+        return render(request, "index.html", {
+            'assignment': asgn,
+            'total_submissions': total_submissions,
+            'submissions_assigned_to_you': submissions_assigned_to_you,
+            'total_students': total_students,
+        })
+    except models.Assignment.DoesNotExist:
+        raise Http404("Assignment not found")
+    
 def submissions(request, assignment_id):
     return render(request, "submissions.html")
 def profile(request):
     return render(request, "profile.html")
 def login_form(request):
     return render(request, "login.html")
+
+def assignment_not_found(request, exception):
+    return render(request, '404.html', status=404)
